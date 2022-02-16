eb42e5fbcbf335c80b6fea2ef1cf63dc5f849df5
Abram Simon <abram@abramsimon.com>
2022-02-16 10:20:34 -0500

    feat(printer): add paper status, online status and remove fs write

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
-from escpos.printer import Network
+from escpos import *
+from PIL import Image
+from io import BytesIO
 import base64
 import os
 
 def print_chit(ip_address, base64_image):
-    path = "output.png"
-    try:
-        with open(path, "wb") as fh:
-            fh.write(base64.decodebytes(base64_image))
-        printer = Network(ip_address)
-        printer.image(path)
-        printer.cut()
-    finally:
-        if os.path.exists(path):
-            os.remove(path)
+    pilImg = Image.open(BytesIO(base64.b64decode(base64_image)))
 
+    lpt = printer.Network(ip_address)
+    print(lpt.paper_status())
+    print(lpt.is_online())
+        
+    lpt.image(pilImg)
+    lpt.cut()
 
 def print_image(printer_ip_address, path):
     with open(path, "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read())
         print_chit(printer_ip_address, encoded_string)
 
-
-print_image("127.0.0.1", "chit.png")
+print_image("192.168.1.40", "chit.png")
